a779cc750617c3998eb12ab2ebff0411e44ef507
ka-studios <155636838+ka-studios@users.noreply.github.com>
2024-05-22 16:02:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,6 +8,26 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('contact-form').addEventListener('submit', (e) => {
         e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const message = document.getElementById('message').value;
+        xhr = new XMLHttpRequest();
+		var url = "https://cors.rare1k.workers.dev/?https://messages.ifukkedyour.mom/send";
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-Type", "application/json");
+		xhr.onreadystatechange = function () {
+    			if (xhr.readyState === 4 && xhr.status === 200) {
+        			//var json = JSON.parse(xhr.responseText);
+        			//console.log(json.email + ", " + json.password);
+    			}
+		};
+		var data = JSON.stringify({
+			"name" : name,
+            "email" : email,
+            "message" : message
+		});
+		xhr.send(data);
         alert('Thank you for your message. We will get back to you soon!');
+        document.getElementById('contact-form').reset();
     });
 });
